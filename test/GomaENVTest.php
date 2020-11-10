43dc02439aa47939c5d77dbc9fa91b1a240cbfe9
Julian Christl <jc@ibpg.eu>
2020-11-10 16:04:34 +0100

    Fixes os dependent directory seperator
    fixed default param

--- test/GomaENVTest.php
+++ test/GomaENVTest.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace Goma\ENV\Test;
 
 use Goma\ENV\GomaENV;
 
 defined("IN_GOMA") or die();
+
 /**
  * Test GomaENV.
  *
  * @package goma/env
  * @author Goma-Team
- * @license	GNU Lesser General Public License, version 3; see "LICENSE.txt"
+ * @license    GNU Lesser General Public License, version 3; see "LICENSE.txt"
  * @copyright Goma-Team
  */
-
 class GomaENVTest extends \GomaUnitTest
 {
     /**
@@ -24,47 +25,63 @@ class GomaENVTest extends \GomaUnitTest
      *   '-user' => '1'
      * )
      */
-    public function testgetCommandLineArgs() {
-        $this->assertEqual(array(
-            "--index" => "--index",
-            "-user" => "1"
-        ), GomaENV::getCommandLineArgs(array(
-            "--index", "-user=1"
-        )));
+    public function testgetCommandLineArgs()
+    {
+        $this->assertEqual(
+            array(
+                "--index" => "--index",
+                "-user"   => "1",
+            ),
+            GomaENV::getCommandLineArgs(
+                array(
+                    "--index",
+                    "-user=1",
+                )
+            )
+        );
     }
 
     /**
      * tests if isPHPUnit returns true in this case.
      */
-    public function testIsPHPUnit() {
+    public function testIsPHPUnit()
+    {
         $this->assertTrue(GomaENV::isPHPUnit());
     }
 
     /**
      * tests if isCommandLineInterface returns true in this case.
      */
-    public function testIsCommandLineInterface() {
+    public function testIsCommandLineInterface()
+    {
         $this->assertTrue(GomaENV::isCommandLineInterface());
     }
 
     /**
      * tests if getRoot works if project is this project and not embedded.
      */
-    public function testGetRootThisProject() {
-        $this->assertEqual("/var/www/", GomaENV::getRoot("/var/www/src"));
+    public function testGetRootThisProject()
+    {
+        $inputRoot = join(DIRECTORY_SEPARATOR, ["", "var", "www", "src"]);
+        $expected = join(DIRECTORY_SEPARATOR, ["", "var", "www", ""]);
+        $this->assertEqual($expected, GomaENV::getRoot($inputRoot));
     }
 
     /**
      * tests if getRoot works if project is embedded.
      */
-    public function testGetRootEmbeddedProject() {
-        $this->assertEqual("/var/www/", GomaENV::getRoot("/var/www/vendor/goma/goma-env/src"));
+    public function testGetRootEmbeddedProject()
+    {
+        $inputRoot = join(DIRECTORY_SEPARATOR, ["", "var", "www", "vendor", "goma", "goma-env", "src"]);
+        $expected = join(DIRECTORY_SEPARATOR, ["", "var", "www", ""]);
+        $this->assertEqual($expected, GomaENV::getRoot($inputRoot));
     }
 
     /**
      * tests if getProjectLevelComposerArray returns an array
      */
-    public function testGetProjectLevelComposerJSON() {
+    public function testGetProjectLevelComposerJSON()
+    {
         $this->assertTrue(is_array(GomaENV::getProjectLevelComposerArray()));
     }
 }
